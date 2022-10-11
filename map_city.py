16e6f8d020d747f9708edc32916139092402648d
VadimNasibullin <vadim-on@mail.ru>
2022-10-11 23:50:41 +0500

    Не могу настроить передачу данных из csv в map_city

--- map_city.py
+++ map_city.py
@@ -1,17 +1,23 @@
 import folium
 from functools import partial
 from geopy.geocoders import Nominatim
+import csv
 
 
 m = folium.Map(location=[59.939099, 30.315877],zoom_start=11)    
 m.save('world.html')
 
-
 geolocator = Nominatim(user_agent='Afro$amuraiNo1')
 geocode = partial(geolocator.geocode,  language='RU')
-location = geolocator.geocode("Тихоокеанская ул., 10, Санкт-Петербург")
-loc = (location.address)
-point = ((location.latitude, location.longitude))
 
-for loc, point in loc, point:
-    folium.Marker(loc, popup=point).add_to(m)
+with open('adress.csv', 'r', encoding='utf-8') as adress:
+    for adress in adress:
+        adr = adress
+        location = geolocator.geocode('adr') #не правильно передает данные из csv
+        info = (location.address)
+        point = ((location.latitude, location.longitude))
+        folium.Marker(point, popup=info).add_to(m)
+        m.save('world.html')
+
+
+
