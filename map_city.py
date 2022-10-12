2631d4b5771095c63c77ba6d1db1cc95ff635ad5
VadimNasibullin <vadim-on@mail.ru>
2022-10-12 21:38:10 +0500

    Принимает список adress.csv в geo_point. Но пока не обрабатывает гео данные

--- map_city.py
+++ map_city.py
@@ -1,18 +1,23 @@
 import folium
 from functools import partial
 from geopy.geocoders import Nominatim
+import csv
 
 
 def snt_peterburg():
     m = folium.Map(location=[59.939099, 30.315877],zoom_start=11)
-    st_peterburg = 'Санкт-Петербург'
-    folium.Marker([59.939099, 30.315877], popup='Санкт-Петербург_',tooltip=st_peterburg).add_to(m)
     m.save('world.html')
 
 def geo_point():
-    geolocator = Nominatim(user_agent='Afro$amuraiNo1')
-    geocode = partial(geolocator.geocode,  language='RU')
-    location = geolocator.geocode('Кузьминское ш. 66, Пушкин') # г. Пушкин, Кузьминское ш. 66, Санкт-Петербург
-    print((location.latitude, location.longitude))
+    with open('adress.csv', 'r', encoding='utf-8') as adress:
+        for adress in adress:
+            adr = adress
+            geolocator = Nominatim(user_agent='Afro$amuraiNo1')
+            geocode = partial(geolocator.geocode,  language='RU')
+            location = geolocator.geocode('Кузьминское ш. 66, Пушкин') # Нужно передать adr
+            point = ((location.latitude, location.longitude))
+            info = (location.address)
+            print(point)
+            print(info)
 
-geo_point()
+#folium.Marker(point, popup=info).add_to(m)
