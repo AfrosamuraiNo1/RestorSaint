35804e6842c0162a099b99d0cb178958e54ecd0d
VadimNasibullin <vadim-on@mail.ru>
2022-10-30 23:15:45 +0500

    Создание таблицы SQlite

---
+++ utils.py
@@ -0,0 +1,18 @@
+import csv
+
+def proces_restaurants_data():
+    with open('data_csv/address.csv', 'r') as f:
+        restaurants = csv.DictReader(f, delimiter=';')
+
+        proces_data = {}
+
+        for restaurant in restaurants:
+            name = restaurant.get('Название заведения')
+            address = restaurant.get('Адрес заведения').split()
+            building_number = address[-1]
+            street_name = ' '.join(address[0:-1])
+
+            if 'r.' not in address:
+                proces_data[name] = f'{building_number} {street_name}, Санкт-Петербург'
+                
+    return proces_data 
