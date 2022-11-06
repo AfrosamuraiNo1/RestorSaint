42a28b91524b8a6bd08af2703fdbfa4c95ab9e69
VadimNasibullin <vadim-on@mail.ru>
2022-11-06 22:28:48 +0500

    сохраняю

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -8,10 +8,21 @@ def create_app():
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
-    @app.route("/")
+    @app.route('/')
     def index():
         title = "Рестораны Санкт-Петербурга"
         name = Place.query.all()
-        return render_template('index.html',title=title, name_list=name)
+        return render_template('index.html', title=title, name_list=name)
     
     return app
+
+
+app = Flask(__name__)
+
+@app.route('/map')
+def map():
+    return render_template('index.html', map='restaurants-spb.html')	
+
+
+if __name__ == '__main__':
+    app.run(debug=True)    
