f6200d165591c5573dedcf0ca64dd9bc4745e6f0
AfrosamuraiNo1 <alexkopenkin777@mail.ru>
2023-01-08 09:48:08 +0300

    Make properties 'latitude and longitude' in data

--- webapp/restaurants/models.py
+++ webapp/restaurants/models.py
@@ -6,6 +6,8 @@ class Place(db.Model):
     info = db.Column(db.String(255),nullable=False)
     address = db.Column(db.String(255),nullable=False)
     description = db.Column(db.Text,nullable=False)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
 
     def __repr__(self):
-        return f'Place: {self.name}{self.info}{self.address}{self.description}'
+        return f'Place: {self.name}{self.info}{self.address}{self.description}{self.latitude}{self.longitude}'
